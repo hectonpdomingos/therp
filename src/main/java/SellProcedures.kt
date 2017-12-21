e3a06fba1c33b9f1a64816d22eca173c02bc7282
Hecton Domingos <hectonpdomingos@gmail.com>
2017-12-21 00:00:46 -0200

    searchInvoiceById and searchInvoiceByDate

--- src/main/java/SellProcedures.kt
+++ src/main/java/SellProcedures.kt
@@ -62,6 +62,32 @@ class Sell{
                     color.RESET
             )
             println()
+        }
+        println("")
+        println("Press any key to back to menu")
+        readLine()
+    }
+
+    fun searchInvoiceAdvanced(firstName: String, lastName: String, invoiceYear: Int, invoiceMonth:Int, invoiceDay: Int){
+        var querySearchInvoiceAdvanced = "SELECT customers.id, customers.firstName, customers.lastName, invoice.id AS \"Invoice\" " + ", invoice.invoiceDate AS \"Date\" ,"  +
+                " invoice_item.product, invoice_item.qty from customers JOIN invoice ON customers.id = invoice.customerId JOIN invoice_item" +
+                " ON invoice_item.invoiceId = invoice.id WHERE 1=1 AND customers.firstName=\"" +firstName+ "\"  AND customers.lastName=\"" +lastName+ "\" " +
+                " AND YEAR(invoice.invoiceDate) = \"" +invoiceYear+ "\" AND MONTH(invoice.invoiceDate) = \"" +invoiceMonth+ "\" " +
+                " AND DAY(invoice.invoiceDate) = \"" + invoiceDay + "\" ;"
+
+        Class.forName(connect.mysqlDriver)
+        var conn: Connection = DriverManager.getConnection(connect.pathConnection)
+        var ps: PreparedStatement = conn.prepareStatement(querySearchInvoiceAdvanced)
+        var rs: ResultSet = ps.executeQuery()
+        while (rs.next()) {
+
+            println(color.COLOR_YELLOW +" -> (Customer ID): " + rs.getString("customers.id") + " (Name ): " + rs.getString("customers.firstName") +
+                    " " + rs.getString("customers.lastName") + " (Invoice ID): " + rs.getString("Invoice") + " (Date): " + rs.getString("Date") +
+                    " (Product): " + rs.getString("product") + " (Qty): "+ rs.getString("qty") +
+
+                    color.RESET
+            )
+            println()
 
         }
         println("")
@@ -69,12 +95,20 @@ class Sell{
         readLine()
     }
 
-    fun searchInvoiceByDate(dateInoice: String){
-        var querySearchInvoiceByFullName = "SELECT customers.id, customers.firstName, customers.lastName, invoice.id, invoice.customerId, invoice.invoiceDate \n" +
-                "from customers left join invoice on customers.id=invoice.customerId WHERE 1=1 AND invoice.invoiceDate=\"" +dateInoice+ "\""
+
+
+
+
+    fun searchInvoiceByDate(invoiceYear: Int, invoiceMonth: Int, InvoiceDay: Int){
+        var querySearchInvoiceByDate = "SELECT customers.id, customers.firstName, customers.lastName, invoice.id, invoice.customerId, invoice.invoiceDate" +
+                " from customers left join invoice on customers.id=invoice.customerId WHERE 1=1 AND" +
+                " YEAR(invoice.invoiceDate) = \"" +invoiceYear+ "\" AND MONTH(invoice.invoiceDate) = \"" +invoiceMonth+ "\" " +
+                " AND DAY(invoice.invoiceDate) = \"" + InvoiceDay + "\" ;"
+
+
         Class.forName(connect.mysqlDriver)
         var conn: Connection = DriverManager.getConnection(connect.pathConnection)
-        var ps: PreparedStatement = conn.prepareStatement(querySearchInvoiceByFullName)
+        var ps: PreparedStatement = conn.prepareStatement(querySearchInvoiceByDate)
         var rs: ResultSet = ps.executeQuery()
         while (rs.next()) {
 
@@ -90,6 +124,34 @@ class Sell{
         println("Press any key to back to menu")
         readLine()
     }
+
+    fun searchInvoiceById(id: Int){
+        var querySearchInvoiceById = "SELECT customers.id, customers.firstName, customers.lastName, invoice.id AS \"Invoice\" " + ", invoice.invoiceDate AS \"Date\" ,"  +
+                " invoice_item.product, invoice_item.qty from customers JOIN invoice ON customers.id = invoice.customerId JOIN invoice_item" +
+                " ON invoice_item.invoiceId = invoice.id WHERE 1=1 AND invoice.id = \""+id+"\" " +
+                " AND invoice_item.invoiceId = \""+id+"\";"
+
+
+
+        Class.forName(connect.mysqlDriver)
+        var conn: Connection = DriverManager.getConnection(connect.pathConnection)
+        var ps: PreparedStatement = conn.prepareStatement(querySearchInvoiceById)
+        var rs: ResultSet = ps.executeQuery()
+        while (rs.next()) {
+
+            println(color.COLOR_YELLOW +" -> (Customer ID): " + rs.getString("customers.id") + " (Name ): " + rs.getString("customers.firstName") +
+                    " " + rs.getString("customers.lastName") + " (Invoice ID): " + rs.getString("Invoice") + " (Date): " + rs.getString("Date") +
+
+                    color.RESET
+            )
+            println()
+
+        }
+        println("")
+        println("Press any key to back to menu")
+        readLine()
+    }
+
 }
 //
 //fun main(args: Array<String>) {
