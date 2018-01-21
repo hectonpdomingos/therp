70e2ee09e080d004fcaab18cdcadebdaa4b21a75
Hecton Domingos <hectonpdomingos@gmail.com>
2018-01-21 17:25:19 -0200

    useTimezone=true&serverTimezone=UTC

--- src/main/java/dbConfig.kt
+++ src/main/java/dbConfig.kt
@@ -1,11 +1,11 @@
 
 class DBConnection {
     val username = "root"
-    val password = "12domlei"
+    val password = ""
     val host = "127.0.0.1"
     val db = "therp"
     val pathConnection: String = "jdbc:mysql://" + this.host + ":3306/" + this.db + "?user=" +
-            this.username + "&password=" + this.password + "&noAccessToProcedureBodies=true"
+            this.username + "&password=" + this.password + "&noAccessToProcedureBodies=true&useTimezone=true&serverTimezone=UTC"
     val mysqlDriver = "com.mysql.cj.jdbc.Driver"
 
 }
